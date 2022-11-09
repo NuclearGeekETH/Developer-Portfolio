3e0c0d27e360ccf9b36bb90d8ee50aa19a09d230
NuclearGeek <47373292+spickett84@users.noreply.github.com>
2022-11-09 13:41:41 -0500

    styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
             <NavBar />
             <div className="header flex light-bg">
                 <div>
-                    <button>Web3 Developer</button>
+                    <div className="green">Web3 Developer</div>
                     <h1 className="white">What are we building today?</h1>
                     <p className="gray">I design and code beautiful simple things, and I love what I do.</p>
                     <a className="green" href="mailto:spickett84@gmail.com">send me an email</a>
