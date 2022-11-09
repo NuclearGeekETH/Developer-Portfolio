ccd804f16a12bee86527497a2842e9f34f6a075d
NuclearGeek <47373292+spickett84@users.noreply.github.com>
2022-11-09 16:28:16 -0500

    update env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {  ALCHEMY_ID: process.env.ALCHEMY_ID  }
+  env: {  ALCHEMY_ID: process.env.ALCHEMY_ID  },
 }
 
 module.exports = nextConfig
