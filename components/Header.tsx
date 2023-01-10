6827424a8fece1e367ac6039f461947f8e65dff9
NuclearGeek <47373292+spickett84@users.noreply.github.com>
2023-01-09 22:06:53 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
                     <p className="gray">I design and code elegant solutions to complex problems and I am passionate about what I do.</p>
                     <a className="green" href="mailto:spickett84@gmail.com">send me an email</a>
                     <div></div>
-                    <a className="green" href="https://aifrens.mypinata.cloud/ipfs/QmbTkUqSJwcZvP7mVkDupngfFQM9KDeCvp8wb3ZC3c526X">view my resume</a>
+                    <a className="green" href="https://aifrens.mypinata.cloud/ipfs/QmbTkUqSJwcZvP7mVkDupngfFQM9KDeCvp8wb3ZC3c526X" target="_blank" rel="noreferrer">view my resume</a>
                 </div>
                 <Image src="/images/pickett.png" width={586.13} height={500} alt="pickett"  /> 
             </div>
