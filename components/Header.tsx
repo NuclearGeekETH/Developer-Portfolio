ab8d2b7e9906f7043e502b9cb3120173e1e0cf98
NuclearGeek <spickett84@yahoo.com>
2023-10-05 11:33:15 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,7 @@ export default function Header() {
             <div className="header flex light-bg">
                 <div>
                     <h1 className="white">What are we building today?</h1>
-                    <p className="gray">I design and code elegant solutions to complex problems and I am passionate about what I do. I have extensive experience building and 
-                        maintaining products in Radiology and Radiation Safety along with Web2 and Web3 frontend and backend design. I love implementing AI and Blockchain into 
-                        my products.</p>
+                    <p className="gray">Crafting thoughtful, elegant solutions to intricate challenges is not just my profession, it’s my passion. My expertise spans across Radiology and Radiation Safety products to Web2 and Web3 design, with a special focus on both front and back end implementation. An avid advocate for disruptive technologies, I enjoy integrating AI and Blockchain into my creations, continuously pushing the boundaries of development and design.</p>
                     <a className="green" href="mailto:spickett84@gmail.com">send me an email</a>
                     <div></div>
                     <a className="green" href="/resume.pdf" target="_blank" rel="noreferrer">view my resume</a>
