60918a1eecf265adbfd2fd93418d01fbbd320770
NuclearGeek <spickett84@yahoo.com>
2023-11-02 22:14:08 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,31 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/itz7kpg.css" />
+        <script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GT-57V3JZX');`,
+      }}>
+      </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11162552213"></script>
+      <script dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-11162552213');`,
+      }}>
+      </script>
       </Head> 
       <body>
         <Main />
         <NextScript />
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GT-57V3JZX
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </Html>
   )
